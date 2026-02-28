0033f8957428f9fad0829d015dcf7721479b4fec
Moizkh3 <khatrimoiz86@gmail.com>
2026-02-28 13:35:44 +0500

    fixing bugs

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -88,12 +88,10 @@ export const login = async (req, res) => {
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
 
         res.cookie('token', token, {
-
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: true,
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000
-
         })
 
         return res.json({ success: true })
@@ -117,7 +115,7 @@ export const logout = async (req, res) => {
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
-        return res.json({ success: true, mesage: 'Logged Out' })
+        return res.json({ success: true, message: 'Logged Out' })
 
     } catch (error) {
 
